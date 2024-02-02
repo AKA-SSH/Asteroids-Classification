448ddee93a576aa78e89a76513713614c7a0a16f
AKA-SSH <akashsingh99.as99@gmail.com>
2024-02-02 21:10:53 +0530

    notebooks created

--- Scripts/data_ingestion.py
+++ Scripts/data_ingestion.py
@@ -1,44 +1,53 @@
-import os
 import sys
-
 import pandas as pd
-
 from utils.logger import logging
 from utils.exception import CustomException
-
 from utils.pickle_file import pickle_file
-from utils.unpickle_file import unpickle_file
+from Scripts.data_preprocessing import DataPreprocessing
 
 class DataIngestion:
-    def __init__(self):
-        pass
-    
-    def data_ingestion(self, raw_data_file_path= r'Data\raw_data.csv'):
+    def data_ingestion(self, raw_data_file_path='Data\\raw_data.csv'):
+        """
+        Performs the data ingestion process for space object data.
+
+        Parameters:
+        - raw_data_file_path (str): The file path to the raw data in CSV format.
+
+        Raises:
+        - CustomException: If an error occurs during the data ingestion process.
+
+        Returns:
+        - tuple: A tuple containing the cleaned features and target data.
+        """
         try:
-            logging.info('data ingestion initiated')
+            logging.info('Data ingestion initiated')
             
-            logging.info(f'loading raw data for {raw_data_file_path}')
-            raw_dataframe= pd.read_csv(raw_data_file_path, low_memory= False)
-            selected_columns= ['orbit_id', 'epoch', 'e', 'i', 'om', 'w', 'ma', 'n', 'class', 'rms', 'neo']
+            logging.info(f'Loading raw data from {raw_data_file_path}')
+            raw_dataframe= pd.read_csv(raw_data_file_path, low_memory=False)
+            selected_columns= ['epoch', 'e', 'i', 'om', 'w', 'ma', 'n', 'class', 'rms', 'neo']
             
-            logging.info('cleaning raw data')
+            logging.info('Cleaning raw data')
             dataframe= raw_dataframe[selected_columns]
 
-            logging.info('creating features and target from cleaned data')
-            features, target= dataframe.drop('neo', axis= 1).copy(), dataframe['neo'].copy()
+            logging.info('Creating features and target from cleaned data')
+            features, target= dataframe.drop('neo', axis=1).copy(), dataframe['neo'].copy()
 
-            logging.info('data ingestion completed')
+            logging.info('Data ingestion completed')
 
-            logging.info('saving features and target data')
-            pickle_file(dataframe= features, file_name= 'features.pkl')
-            pickle_file(dataframe= target, file_name= 'target.pkl')
-            logging.info('contents saved')
+            logging.info('Saving features and target data')
+            pickle_file(object=features, file_name='features.pkl')
+            pickle_file(object=target, file_name='target.pkl')
+            logging.info('Contents saved')
         
         except Exception as CE:
-            logging.error(f'error during data ingestion: {str(CE)}', exc_info= True)
+            logging.error(f'Error during data ingestion: {str(CE)}', exc_info=True)
             raise CustomException(CE, sys)
 
 if __name__ == '__main__':
-    # data ingestion
-    object= DataIngestion()
-    cleaned_dataframe= object.data_ingestion()
+    # Data ingestion
+    data_ingestion_object= DataIngestion()
+    data_ingestion_object.data_ingestion()
+
+    # Data preprocessing
+    data_preprocessing_object= DataPreprocessing()
+    data_preprocessing_object.data_preprocessing(features_file_path='artifacts\\features.pkl', target_file_path='artifacts\\target.pkl')
---
+++ Scripts/data_preprocessing.py
@@ -0,0 +1,64 @@
+import sys
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from utils.logger import logging
+from utils.exception import CustomException
+from utils.pickle_file import pickle_file
+from utils.unpickle_file import unpickle_file
+
+class DataPreprocessing:
+    def __init__(self):
+        pass
+
+    def data_preprocessing(self, features_file_path, target_file_path):
+        """
+        Performs data preprocessing for machine learning tasks.
+
+        Parameters:
+        - features_file_path (str): File path to the pickled file containing features.
+        - target_file_path (str): File path to the pickled file containing target labels.
+
+        Raises:
+        - CustomException: If an error occurs during the data preprocessing process.
+
+        Returns:
+        None
+        """
+        try:
+            logging.info('Data preprocessing initiated')
+            
+            logging.info('Loading data')
+            features = unpickle_file(features_file_path)
+            target = unpickle_file(target_file_path)
+
+            logging.info('Splitting numerical and categorical features')
+            numerical_columns = features.select_dtypes(exclude='O').columns
+            categorical_columns = features.select_dtypes(include='O').columns
+
+            logging.info('Splitting train and test data')
+            train_features, test_features, train_target, test_target = train_test_split(features, target, test_size=0.2, random_state=42)
+
+            logging.info('Performing label encoding on categorical features')
+            LE= LabelEncoder()
+            for column in categorical_columns:
+                 train_features[column]= LE.fit_transform(train_features[column])
+                 test_features[column]= LE.transform(test_features[column])
+
+            pickle_file(object=LE, file_name='label_encoder.pkl')
+
+            logging.info('Encoding target label')
+            train_target = train_target.map({'N': 0, 'Y': 1})
+            test_target = test_target.map({'N': 0, 'Y': 1})
+
+            logging.info('Data preprocessing completed')
+            logging.info('Saving train and test data')
+
+            pickle_file(object=train_features, file_name='train_features.pkl')
+            pickle_file(object=train_target, file_name='train_target.pkl')
+            pickle_file(object=test_features, file_name='test_features.pkl')
+            pickle_file(object=test_target, file_name='test_target.pkl')
+
+        except Exception as CE:
+            logging.error(f'Error during data preprocessing: {str(CE)}', exc_info=True)
+            raise CustomException(CE, sys)
