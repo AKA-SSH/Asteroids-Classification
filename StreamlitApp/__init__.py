558e0c6c1311bfa65ac5e99f43bed86662cf99de
AKA-SSH <akashsingh99.as99@gmail.com>
2024-02-04 17:56:50 +0530

    file path updated

--- StreamlitApp/__init__.py
+++
@@ -1,9 +0,0 @@
-# __init__.py
-
-# You can define variables or configurations here
-PACKAGE_NAME = "StreamlitApp"
-
-from . import app
-from . import data_processing_pipeline
-from . import model_training_pipeline
-from . import prediction_pipeline
